b0991324aaa22d4f3488b81c435b00945d91962a
Zevi Liao <jointjm@gmail.com>
2020-03-16 00:26:01 +0800

    udpate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,56 @@
 <template>
-    <div class="hello">
-
-        <button @click="onSubmit">click mockjs</button>
-
-    </div>
+  <div class="hello">
+    <button @click="onSubmit">click mockjs</button>
+  </div>
 </template>
 
 <script>
-    /* eslint-disable */
+/* eslint-disable */
 
-    import axios from 'axios'
-    import './mock'
-    // import './mock-ax'
+import axios from "axios";
+import "./mock";
+// import './mock-ax'
 
-
-    export default {
-        name: 'HelloWorld',
-        props: {
-            msg: String
-        },
-        data() {
-            return {
-                data: null
-            }
-        },
-        methods: {
-            onSubmit() {
-                axios.get('https://jsonplaceholder.typicode.com/todos/1').then(res => {
-                    console.log(res.data);
-                })
-            }
-        }
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      data: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+        console.log(res.data);
+      });
+      axios.get("https://jsonplaceholder.typicode.com/todos/2").then(res => {
+        console.log(res.data);
+      });
     }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
+h3 {
+  margin: 40px 0 0;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-    a {
-        color: #42b983;
-    }
+a {
+  color: #42b983;
+}
 </style>
--- src/components/mock-ax.js
+++ src/components/mock-ax.js
@@ -1,14 +1,21 @@
 /* eslint-disable */
-import axios from 'axios'
-import MockAdapter from 'axios-mock-adapter'
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
-let mock = new MockAdapter(axios)
+let mock = new MockAdapter(axios);
+mock.onGet(/\/todos\/1/).reply(200, {
+  users: [
+    { id: 1, name: "John Smith" },
+    { id: 2, name: "John Doe" }
+  ]
+});
 
-export default mock.onGet(/\/todos\/1/)
-    .reply(200, {
-        users: [
-            {id: 1, name: 'John Smith'},
-            {id: 2, name: 'John Doe'}
-        ]
-    })
+mock.onGet(/\/todos\/2/).reply(200, {
+  users: [
+    { id: 1, name: "John Smith 2" },
+    { id: 2, name: "John Doe" }
+  ]
+});
 
+
+export default mock;
